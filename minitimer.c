4041660366450b9bf9fa1a10c880ce3b9b22b7ee
Ariadna Vigo <arivigodr@gmail.com>
2020-02-09 23:44:35 +0100

    Pausing timer implemented via threads

--- minitimer.c
+++ minitimer.c
@@ -14,41 +14,65 @@
 */
 
 #include <ncurses.h>
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-static void time_dec(int *hrs, int *mins, int *secs);
-static int parse_time(char *time_str, int *hrs, int *mins, int *secs);
-static void printw_center(const char *fmt, ...);
-static void start_loop(int hrs, int mins, int secs);
-
-static void time_dec(int *hrs, int *mins, int *secs)
+struct time
 {
-    --*secs;
-    if(*secs < 0)
+    int hrs;
+    int mins;
+    int secs;
+};
+
+static int time_zero(struct time the_time);
+static void time_dec(struct time *the_time);
+static int parse_time(char *time_str, struct time *the_time);
+static void printw_center(const char *fmt, ...);
+static void *timer_loop(void *arg);
+
+static pthread_mutex_t timer_runs_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int timer_runs = 0;
+
+static int time_zero(struct time the_time)
+{
+    if((the_time.hrs == 0) && (the_time.mins == 0) && (the_time.secs == 0))
     {
-        *secs = 59;
-        --*mins;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+    
+static void time_dec(struct time *the_time)
+{
+    --the_time->secs;
+    if(the_time->secs < 0)
+    {
+        the_time->secs = 59;
+        --the_time->mins;
     }
 
-    if(*mins < 0)
+    if(the_time->mins < 0)
     {
-        *mins = 59;
-        --*hrs;
+        the_time->mins = 59;
+        --the_time->hrs;
     }
 }
 
-static int parse_time(char *time_str, int *hrs, int *mins, int *secs)
+static int parse_time(char *time_str, struct time *the_time)
 {
     char *strptr = strtok(time_str, ":");
 
     char *errptr = NULL;
     
     /* Hours */
-    *hrs = strtoul(strptr, &errptr, 10);
+    the_time->hrs = strtoul(strptr, &errptr, 10);
     if(*errptr)
     {
         return -1;
@@ -56,7 +80,7 @@ static int parse_time(char *time_str, int *hrs, int *mins, int *secs)
 
     /* Minutes */
     strptr = strtok(NULL, ":");
-    *mins = strtoul(strptr, &errptr, 10);
+    the_time->mins = strtoul(strptr, &errptr, 10);
     if(*errptr)
     {
         return -1;
@@ -64,7 +88,7 @@ static int parse_time(char *time_str, int *hrs, int *mins, int *secs)
 
     /* Secs */
     strptr = strtok(NULL, "\0");
-    *secs = strtoul(strptr, &errptr, 10);
+    the_time->secs = strtoul(strptr, &errptr, 10);
     if(*errptr)
     {
         return -1;
@@ -93,46 +117,85 @@ static void printw_center(const char *fmt, ...)
     mvaddstr(vindent, xindent, text);
 }
     
-static void start_loop(int hrs, int mins, int secs)
+static void *timer_loop(void *ptr)
 {
-    initscr();
-    curs_set(0);
+    struct time *the_time = ptr;
 
-    while((hrs > 0) || (mins > 0) || (secs > 0))
+    timer_runs = 1;
+
+    while(!time_zero(*the_time))
     {
         clear();
-        printw_center("%02d:%02d:%02d", hrs, mins, secs);
-        time_dec(&hrs, &mins, &secs);
+        printw_center("%02d:%02d:%02d", the_time->hrs, the_time->mins, the_time->secs);
         refresh();
-        sleep(1);
+        if(timer_runs)
+        {
+            time_dec(the_time);
+            sleep(1);
+        }
     }
 
     clear();
     printw_center("Time's up!");
     getch();
-    
-    endwin();
+
+    return the_time;
+}
+
+static void *cmd_loop(void *ptr)
+{
+    struct time *the_time = ptr;
+    while(!time_zero(*the_time))
+    {
+        int cmd = getch();
+        if(cmd == ' ')
+        {
+            pthread_mutex_lock(&timer_runs_mutex);
+            timer_runs = timer_runs ^ 1; // XOR'ing
+            pthread_mutex_unlock(&timer_runs_mutex);
+        }
+    }
+
+    return the_time;
 }
 
 int main(int argc, char **argv)
-{               
+{
     if(argc < 2)
     {
         printf("Time required\n");
         return -1;
     }
 
-    int hrs = 0;
-    int mins = 0;
-    int secs = 0;
-    int status = parse_time(argv[1], &hrs, &mins, &secs);
+    struct time the_time;
+    memset(&the_time, 0, sizeof(struct time));
+    int status = parse_time(argv[1], &the_time);
     if(status)
     {
         printf("Invalid or ill-formed time\n");
         return -1;
     }
 
-    start_loop(hrs, mins, secs);
+    initscr();
+    noecho();
+    curs_set(0);
 
+    pthread_t timer_thr, cmd_thr;
+    int timer_stat = pthread_create(&timer_thr, NULL, timer_loop, (void *)&the_time);
+    int cmd_stat = pthread_create(&cmd_thr, NULL, cmd_loop, (void *)&the_time);
+    
+    if(!timer_stat || !cmd_stat)
+    {
+        pthread_join(timer_thr, NULL);
+        pthread_join(cmd_thr, NULL);
+    }
+    else
+    {
+        printf("Unexpected error. Aborting.");
+        endwin();
+        return -1;
+    }
+    
+    endwin();
     return 0;
 }
