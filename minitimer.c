d9bd1c7ca9c9d82a349df848a6d18e438b2cbb8a
Ariadna Vigo <arivigodr@gmail.com>
2020-03-07 18:08:06 +0100

    Tabifying code

--- minitimer.c
+++ minitimer.c
@@ -2,14 +2,14 @@
    Copyright 2020 Rubén Santos <ribal@cocaine.ninja>
 
    Licensed under the Apache License, Version 2.0 (the "License"); you may not
-   use this file except in compliance with the License.  You may obtain a copy
+   use this file except in compliance with the License.	 You may obtain a copy
    of the License at
 
    http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	 See the
    License for the specific language governing permissions and limitations under
    the License.
 */
@@ -26,9 +26,9 @@
 
 struct time
 {
-    int hrs;
-    int mins;
-    int secs;
+	int hrs;
+	int mins;
+	int secs;
 };
 
 static int time_zero(struct time the_time);
@@ -44,180 +44,180 @@ static int timer_runs = 0;
 
 static int time_zero(struct time the_time)
 {
-    if((the_time.hrs == 0) && (the_time.mins == 0) && (the_time.secs == 0))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+	if((the_time.hrs == 0) && (the_time.mins == 0) && (the_time.secs == 0))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 static void time_dec(struct time *the_time)
 {
-    --the_time->secs;
-    if(the_time->secs < 0)
-    {
-        the_time->secs = 59;
-        --the_time->mins;
-    }
+	--the_time->secs;
+	if(the_time->secs < 0)
+	{
+		the_time->secs = 59;
+		--the_time->mins;
+	}
 
-    if(the_time->mins < 0)
-    {
-        the_time->mins = 59;
-        --the_time->hrs;
-    }
+	if(the_time->mins < 0)
+	{
+		the_time->mins = 59;
+		--the_time->hrs;
+	}
 }
 
 static int parse_time(char *time_str, struct time *the_time)
 {
-    char *strptr = strtok(time_str, ":");
+	char *strptr = strtok(time_str, ":");
 
-    char *errptr = NULL;
+	char *errptr = NULL;
 
-    /* Hours */
-    the_time->hrs = strtoul(strptr, &errptr, 10);
-    if(*errptr)
-    {
-        return -1;
-    }
+	/* Hours */
+	the_time->hrs = strtoul(strptr, &errptr, 10);
+	if(*errptr)
+	{
+		return -1;
+	}
 
-    /* Minutes */
-    strptr = strtok(NULL, ":");
-    the_time->mins = strtoul(strptr, &errptr, 10);
-    if(*errptr)
-    {
-        return -1;
-    }
+	/* Minutes */
+	strptr = strtok(NULL, ":");
+	the_time->mins = strtoul(strptr, &errptr, 10);
+	if(*errptr)
+	{
+		return -1;
+	}
 
-    /* Secs */
-    strptr = strtok(NULL, "\0");
-    the_time->secs = strtoul(strptr, &errptr, 10);
-    if(*errptr)
-    {
-        return -1;
-    }
+	/* Secs */
+	strptr = strtok(NULL, "\0");
+	the_time->secs = strtoul(strptr, &errptr, 10);
+	if(*errptr)
+	{
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
 static void printw_center(const char *fmt, ...)
 {
-    va_list ap;
-    va_start(ap, fmt);
-    char text[PRINTW_BUF_SIZE];
-    memset(text, 0, sizeof(text));
-    vsnprintf(text, sizeof(text), fmt, ap);
-    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	char text[PRINTW_BUF_SIZE];
+	memset(text, 0, sizeof(text));
+	vsnprintf(text, sizeof(text), fmt, ap);
+	va_end(ap);
 
-    int y = 0;
-    int x = 0;
-    getmaxyx(stdscr, y, x);
+	int y = 0;
+	int x = 0;
+	getmaxyx(stdscr, y, x);
 
-    int len = strlen(text);
-    int xindent = (x - len) / 2;
-    int yindent = (y - 1) / 2;
+	int len = strlen(text);
+	int xindent = (x - len) / 2;
+	int yindent = (y - 1) / 2;
 
-    mvaddstr(yindent, xindent, text);
+	mvaddstr(yindent, xindent, text);
 }
 
 static void printw_bottom(const char *fmt, ...)
 {
-    va_list ap;
-    va_start(ap, fmt);
-    char text[PRINTW_BUF_SIZE];
-    memset(text, 0, sizeof(text));
-    vsnprintf(text, sizeof(text), fmt, ap);
-    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	char text[PRINTW_BUF_SIZE];
+	memset(text, 0, sizeof(text));
+	vsnprintf(text, sizeof(text), fmt, ap);
+	va_end(ap);
 
-    int y = 0;
-    int x = 0;
-    getmaxyx(stdscr, y, x);
+	int y = 0;
+	int x = 0;
+	getmaxyx(stdscr, y, x);
 
-    int len = strlen(text);
-    int xindent = (x - len) / 2;
-    int yindent = y - 1;
+	int len = strlen(text);
+	int xindent = (x - len) / 2;
+	int yindent = y - 1;
 
-    mvaddstr(yindent, xindent, text);
+	mvaddstr(yindent, xindent, text);
 }
 
 static void ui_update(struct time the_time)
 {
-    clear();
+	clear();
 
-    printw_center("%02d:%02d:%02d", the_time.hrs, the_time.mins, the_time.secs);
+	printw_center("%02d:%02d:%02d", the_time.hrs, the_time.mins, the_time.secs);
 
-    if(!time_zero(the_time))
-    {
-        printw_bottom("[Space] = pause/resume\t[q] = Stop");
-    }
-    else
-    {
-        printw_bottom("Press any key to exit.");
-    }
+	if(!time_zero(the_time))
+	{
+		printw_bottom("[Space] = pause/resume\t[q] = Stop");
+	}
+	else
+	{
+		printw_bottom("Press any key to exit.");
+	}
 
-    refresh();
+	refresh();
 }
 
 static int kbhit(void)
 {
-    struct timeval tv = { 0L, 0L};
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(0, &fds);
-    return select(1, &fds, NULL, NULL, &tv);
+	struct timeval tv = { 0L, 0L};
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(0, &fds);
+	return select(1, &fds, NULL, NULL, &tv);
 }
 
 static void poll_event(void)
 {
-    if (kbhit())
-    {
-        switch (tolower(getch())) {
-        case ' ':
-            timer_runs = timer_runs ^ 1; // XOR'ing
-            break;
-        case 'q':
-            timer_runs = -1;
-            break;
-        }
-    }
+	if (kbhit())
+	{
+		switch (tolower(getch())) {
+		case ' ':
+			timer_runs = timer_runs ^ 1; // XOR'ing
+			break;
+		case 'q':
+			timer_runs = -1;
+			break;
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
-    if(argc < 2)
-    {
-        printf("Time required\n");
-        return -1;
-    }
+	if(argc < 2)
+	{
+		printf("Time required\n");
+		return -1;
+	}
 
-    struct time the_time;
-    memset(&the_time, 0, sizeof(struct time));
-    int status = parse_time(argv[1], &the_time);
-    if(status)
-    {
-        printf("Invalid or ill-formed time\n");
-        return -1;
-    }
+	struct time the_time;
+	memset(&the_time, 0, sizeof(struct time));
+	int status = parse_time(argv[1], &the_time);
+	if(status)
+	{
+		printf("Invalid or ill-formed time\n");
+		return -1;
+	}
 
-    initscr();
-    noecho();
-    curs_set(0);
+	initscr();
+	noecho();
+	curs_set(0);
 
-    timer_runs = 1;
-    while (!time_zero(the_time) && timer_runs != -1)
-    {
-        poll_event();
-        if(timer_runs == 1)
-        {
-            time_dec(&the_time);
-            ui_update(the_time);
-            sleep(1);
-        }
-    }
+	timer_runs = 1;
+	while (!time_zero(the_time) && timer_runs != -1)
+	{
+		poll_event();
+		if(timer_runs == 1)
+		{
+			time_dec(&the_time);
+			ui_update(the_time);
+			sleep(1);
+		}
+	}
 
-    endwin();
+	endwin();
 
-    return 0;
+	return 0;
 }
