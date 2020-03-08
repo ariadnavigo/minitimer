7a34a0d441dca82a7a51d17f860b6e8bef319e74
Ariadna Vigo <arivigodr@gmail.com>
2020-03-08 15:18:17 +0100

    Code reformatted

--- minitimer.c
+++ minitimer.c
@@ -24,8 +24,7 @@
 #include <string.h>
 #include <unistd.h>
 
-struct time
-{
+struct time {
 	int hrs;
 	int mins;
 	int secs;
@@ -42,35 +41,32 @@ static void poll_event(void);
 
 static int timer_runs = 0;
 
-static int time_zero(struct time the_time)
+static int
+time_zero(struct time the_time)
 {
-	if((the_time.hrs == 0) && (the_time.mins == 0) && (the_time.secs == 0))
-	{
+	if ((the_time.hrs == 0) && (the_time.mins == 0) && (the_time.secs == 0))
 		return 1;
-	}
 	else
-	{
 		return 0;
-	}
 }
 
-static void time_dec(struct time *the_time)
+static void
+time_dec(struct time *the_time)
 {
 	--the_time->secs;
-	if(the_time->secs < 0)
-	{
+	if (the_time->secs < 0) {
 		the_time->secs = 59;
 		--the_time->mins;
 	}
 
-	if(the_time->mins < 0)
-	{
+	if (the_time->mins < 0) {
 		the_time->mins = 59;
 		--the_time->hrs;
 	}
 }
 
-static int parse_time(char *time_str, struct time *the_time)
+static int
+parse_time(char *time_str, struct time *the_time)
 {
 	char *strptr = strtok(time_str, ":");
 
@@ -78,31 +74,26 @@ static int parse_time(char *time_str, struct time *the_time)
 
 	/* Hours */
 	the_time->hrs = strtoul(strptr, &errptr, 10);
-	if(*errptr)
-	{
+	if (*errptr)
 		return -1;
-	}
 
 	/* Minutes */
 	strptr = strtok(NULL, ":");
 	the_time->mins = strtoul(strptr, &errptr, 10);
-	if(*errptr)
-	{
+	if (*errptr)
 		return -1;
-	}
 
 	/* Secs */
 	strptr = strtok(NULL, "\0");
 	the_time->secs = strtoul(strptr, &errptr, 10);
-	if(*errptr)
-	{
+	if (*errptr)
 		return -1;
-	}
 
 	return 0;
 }
 
-static void printw_center(const char *fmt, ...)
+static void
+printw_center(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
@@ -122,7 +113,8 @@ static void printw_center(const char *fmt, ...)
 	mvaddstr(yindent, xindent, text);
 }
 
-static void printw_bottom(const char *fmt, ...)
+static void
+printw_bottom(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
@@ -142,37 +134,35 @@ static void printw_bottom(const char *fmt, ...)
 	mvaddstr(yindent, xindent, text);
 }
 
-static void ui_update(struct time the_time)
+static void
+ui_update(struct time the_time)
 {
 	clear();
 
 	printw_center("%02d:%02d:%02d", the_time.hrs, the_time.mins, the_time.secs);
 
-	if(!time_zero(the_time))
-	{
+	if (!time_zero(the_time))
 		printw_bottom("[Space] = pause/resume\t[q] = Stop");
-	}
 	else
-	{
 		printw_bottom("Press any key to exit.");
-	}
 
 	refresh();
 }
 
-static int kbhit(void)
+static int
+kbhit(void)
 {
-	struct timeval tv = { 0L, 0L};
+	struct timeval tv = { 0L, 0L };
 	fd_set fds;
 	FD_ZERO(&fds);
 	FD_SET(0, &fds);
 	return select(1, &fds, NULL, NULL, &tv);
 }
 
-static void poll_event(void)
+static void
+poll_event(void)
 {
-	if (kbhit())
-	{
+	if (kbhit()) {
 		switch (tolower(getch())) {
 		case ' ':
 			timer_runs = timer_runs ^ 1; // XOR'ing
@@ -184,10 +174,10 @@ static void poll_event(void)
 	}
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-	if(argc < 2)
-	{
+	if (argc < 2) {
 		printf("Time required\n");
 		return -1;
 	}
@@ -195,8 +185,7 @@ int main(int argc, char **argv)
 	struct time the_time;
 	memset(&the_time, 0, sizeof(struct time));
 	int status = parse_time(argv[1], &the_time);
-	if(status)
-	{
+	if (status) {
 		printf("Invalid or ill-formed time\n");
 		return -1;
 	}
@@ -206,11 +195,9 @@ int main(int argc, char **argv)
 	curs_set(0);
 
 	timer_runs = 1;
-	while (!time_zero(the_time) && timer_runs != -1)
-	{
+	while (!time_zero(the_time) && timer_runs != -1) {
 		poll_event();
-		if(timer_runs == 1)
-		{
+		if (timer_runs == 1) {
 			time_dec(&the_time);
 			ui_update(the_time);
 			sleep(1);
